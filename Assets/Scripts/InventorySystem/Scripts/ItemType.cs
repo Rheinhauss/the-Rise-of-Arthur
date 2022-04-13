4e08d430e0969f051a25074eaecfb596ff72e0f2
ssh555 <89600078+ssh555@users.noreply.github.com>
2022-04-13 16:50:31 +0800

    优化添加itemtype，完善item图片

--- Assets/Scripts/InventorySystem/Scripts/ItemType.cs
+++ Assets/Scripts/InventorySystem/Scripts/ItemType.cs
@@ -10,17 +10,17 @@ public enum ItemType
 {
     //头部
     [HideInInspector]
-    Head_Armor,
+    Head_Armor = 0,
     [LabelText("头盔A")]
     Head_Armor_A,
     [LabelText("头盔B")]
     Head_Armor_B,
     [HideInInspector]
-    Head_Armor_End,
+    Head_Armor_End = 50,
 
     //躯干
     [HideInInspector]
-    Torso_Armor,
+    Torso_Armor = 51,
     [LabelText("皮革甲")]
     Torso_Armor_A,
     [LabelText("锁子甲")]
@@ -28,31 +28,31 @@ public enum ItemType
     [LabelText("板甲")]
     Torso_Armor_C,
     [HideInInspector]
-    Torso_Armor_End,
+    Torso_Armor_End = 100,
 
     //腿
     [HideInInspector]
-    Leg_Armor,
+    Leg_Armor = 101,
     [LabelText("腿部盔甲A")]
     Leg_Armor_A,
     [LabelText("腿部盔甲B")]
     Leg_Armor_B,
     [HideInInspector]
-    Leg_Armor_End,
+    Leg_Armor_End = 150,
 
     //脚
     [HideInInspector]
-    Foot_Armor,
+    Foot_Armor = 151,
     [LabelText("鞋子A")]
     Foot_Armor_A,
     [LabelText("鞋子B")]
     Foot_Armor_B,
     [HideInInspector]
-    Foot_Armor_End,
+    Foot_Armor_End = 200,
 
     //武器
     [HideInInspector]
-    Weapon,
+    Weapon = 201,
     [LabelText("锈迹斑斑的剑")]
     Weapon_A,
     [LabelText("军用品剑")]
@@ -60,33 +60,33 @@ public enum ItemType
     [LabelText("精制品剑")]
     Weapon_C,
     [HideInInspector]
-    Weapon_End,
+    Weapon_End = 250,
 
     //HP
     [HideInInspector]
-    HealthPotion,
+    HealthPotion = 251,
     [LabelText("小型HP药剂")]
     HealthPotion_Little,
     [LabelText("大型HP药剂")]
     HealthPotion_Big,
     [HideInInspector]
-    HealthPotion_End,
+    HealthPotion_End = 300,
 
     //MP
     [HideInInspector]
-    ManaPotion,
+    ManaPotion = 301,
     [LabelText("小型MP药剂")]
     ManaPotion_Little,
     [HideInInspector]
-    ManaPotion_End,
+    ManaPotion_End = 350,
 
     //Coin
     [HideInInspector]
-    Coin,
+    Coin = 351,
     [LabelText("金币")]
     Coin_A,
     [HideInInspector]
-    Coin_End,
+    Coin_End = 400,
 
     None,
 }
