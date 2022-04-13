bb467b6be6eed1a647cde50d8e1998c190a464d1
ssh555 <89600078+ssh555@users.noreply.github.com>
2022-04-13 15:15:23 +0800

    更改摄像机，新添解谜旋转方式

--- Assets/Puzzles/Puzzle01/Scripts/TransObjectEntity.cs
+++ Assets/Puzzles/Puzzle01/Scripts/TransObjectEntity.cs
@@ -9,6 +9,27 @@ public class TransObjectEntity : MonoBehaviour
 
     private void OnMouseDown()
     {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
         m_transform.Rotate(rot);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            UnitControllerComponent.inputComponent.BindInputAction(KeyCode.F, Rotate, KeyCodeType.DOWN);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UnitControllerComponent.inputComponent.UnBindInputAction(KeyCode.F, Rotate, KeyCodeType.DOWN);
+        }
     }
 }
